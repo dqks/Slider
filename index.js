dd469feb3e3615503e485f1a8c081d925d105532
Maxim Shtin <maksimshtin@gmail.com>
2022-09-25 16:06:56 +0500

    make the slider better

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const img = document.querySelector('.main')
-let arr = [1, 2, 3]
-let n = 0
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const img = document.querySelector('.main');
+let arr = [1, 2, 3];
+let n = 0;
 let images = [
     firstImage = {
-        src: '1.jpg'
+        src: 'img/1.jpg'
     },
     secondImage = {
-        src: '2.jpg'
+        src: 'img/2.jpg'
     },
     thirdImage = {
-        src: '3.jpg'
+        src: 'img/3.jpg'
     } 
 ]
 
-
-
 right.addEventListener("click", () => {
-    if (img.src != '1.jpg' && n < images.length) {
-        n++
-        }
+    // if (img.src != 'img/1.jpg') {
+    //     n++
+    //     }
+n++
 img.src = images[n].src
+console.log(n)
+return n 
+})
+
+left.addEventListener('click', () => {
+    n--
+    img.src = images[n].src
+    console.log(n)
 })
 
